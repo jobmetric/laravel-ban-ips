d68687f57b68e1eec22c3b7ef95ec8e21e99f208
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-01 14:12:22 +0330

    add store request file

---
+++ src/Http/Requests/StoreBanIpRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace JobMetric\BanIp\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use JobMetric\BanIp\Rules\BanIpExistRule;
+
+class StoreBanIpRequest extends FormRequest
+{
+    public array $data = [];
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        if(empty($this->data)) {
+            $type = $this->input('type');
+        } else {
+            $type = $this->data['type'] ?? null;
+        }
+
+        return [
+            'ip' => [
+                'string',
+                new BanIpExistRule($type)
+            ],
+            'type' => 'string',
+            'reason' => 'string',
+            'banned_at' => 'date',
+            'expired_at' => 'date|after:banned_at',
+        ];
+    }
+
+    /**
+     * Set data for validation
+     *
+     * @param array $data
+     * @return static
+     */
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
