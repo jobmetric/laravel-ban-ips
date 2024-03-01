710fc2b270cfd3690c9c64aaf69e853d85b91067
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-01 14:10:25 +0330

    add language file

---
+++ lang/en/base.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Ban Ip Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Ban Ip for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'validation' => [
+        'errors' => 'Validation errors occurred.',
+        'ban_ip_exist' => 'The :attribute (:ip) exist in type (:type).',
+        'ban_ip_not_found' => 'The ban ip not found.',
+    ],
+
+    'messages' => [
+        'created' => 'The ban ip was created successfully.',
+        'updated' => 'The ban ip was updated successfully.',
+        'deleted' => 'The ban ip was deleted successfully.',
+    ],
+
+];
