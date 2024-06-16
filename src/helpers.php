6e0693715779267189c1e83471745839305bd0a9
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-17 01:31:13 +0330

    add helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,45 @@
 <?php
+
+use JobMetric\BanIp\BanIp;
+use JobMetric\BanIp\Facades\BanIp as BanIpFacade;
+
+if (!function_exists('storeBanIp')) {
+    /**
+     * Get the BanIp instance.
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    function storeBanIp(array $data): array
+    {
+        return BanIpFacade::store($data);
+    }
+}
+
+if (!function_exists('updateBanIp')) {
+    /**
+     * Get the BanIp instance.
+     *
+     * @param int $ban_ip_id
+     * @param array $data
+     *
+     * @return array
+     */
+    function updateBanIp(int $ban_ip_id, array $data = []): array
+    {
+        return BanIpFacade::update($ban_ip_id, $data);
+    }
+}
+
+if (!function_exists('deleteExpiredBanIp')) {
+    /**
+     * Get the BanIp instance.
+     *
+     * @return bool
+     */
+    function deleteExpiredBanIp(): bool
+    {
+        return BanIpFacade::deleteExpired();
+    }
+}
